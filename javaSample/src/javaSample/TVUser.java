6997bcd2168a6675113c8b3f22048f07258b3af6
ked1125 <ked1125@naver.com>
2024-06-17 11:37:23 +0900

    tv

--- javaSample/src/javaSample/TVUser.java
+++ javaSample/src/javaSample/TVUser.java
@@ -3,8 +3,21 @@ package javaSample;
 public class TVUser {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+//		System.out.println("test");
 
+//		LgTv tv = new LgTv() ;			//new입력후 ctrl+spacebar
+//		tv.turnOn() ;					//ctrl+alt+↓ 행복사
+//		tv.soundOn() ;
+//		tv.soundOff();
+//		tv.turnOff() ;
+
+//		Tv tv = new SamsungTv() ;
+		
+		SamsungTv tv = new SamsungTv();
+		tv.powerOn() ;
+		tv.volumeOn() ;
+		tv.volumeOff() ;
+		tv.powerOff() ;
 	}
 
 }

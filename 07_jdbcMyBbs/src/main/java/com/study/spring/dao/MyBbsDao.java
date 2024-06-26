036dd0fb0c5b93104776aef8bad6931360864e35
ked1125 <ked1125@naver.com>
2024-06-26 13:12:56 +0900

    배포까지연습..:<

--- 07_jdbcMyBbs/src/main/java/com/study/spring/dao/MyBbsDao.java
+++ 07_jdbcMyBbs/src/main/java/com/study/spring/dao/MyBbsDao.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Repository;
 
 import com.study.spring.dto.MyBbsDto;
 
-@Repository
+@Repository										// @Repository 붙어있으면 bean이라고 봐도 됨!
+// Repository 안에 @Component 들어있고 => @Component가 붙으면 스프링이 맨처음에 쫙 스캔하면서 bean 등록함..^^;;;;
+
 public class MyBbsDao implements IMyBbsDao{
 	
 	@Autowired
@@ -36,7 +38,22 @@ public class MyBbsDao implements IMyBbsDao{
 		return dto;
 	}
 	
+	@Override
+	public int deleteDao(String id) {
+		String query = "delete from mybbs1 where id = ?";	// ? 해놓으면 매개변수 순서대로 들어간다고 하심 
+//		String query = "delete from mybbs1 where id = ? , ? , ?";	// ? 해놓으면 매개변수 순서대로 들어간다고 하심 
+		
+		return jdbcTemplate.update(
+				query, Integer.parseInt(id)
+				);
+	}
 	
+	@Override
+	public int writeDao(String writer, String title, String content) {
+		String query = "insert into mybbs1(writer,title,content)"		// 한줄로 쓰기 너무 기니까 분할해줌. 쭉써도댐 ㅎㅎㅎ
+				+ " values (?,?,?) ";									// 매개변수 순서대로 차례차례 들어감
+		return jdbcTemplate.update(query, writer, title, content);
+	}
 	
 	
 
--- 07_jdbcMyBbs/src/main/webapp/WEB-INF/views/list.jsp
+++ 07_jdbcMyBbs/src/main/webapp/WEB-INF/views/list.jsp
@@ -10,7 +10,8 @@
 <body>
 	<h2>집가고싶어</h2>
 	<c:forEach var="dto" items="${lists}">
-		<div>${dto.id} / ${dto.writer} / <a href="/view?id=${dto.id}">${dto.title}</a> / ${dto.content} </div>
+		<div>${dto.id} / ${dto.writer} / <a href="./view?id=${dto.id}">${dto.title}</a> / ${dto.content} / <a href="./delete?id=${dto.id}">삭제</a> </div>
 	</c:forEach>
+	<a href="./writerForm">글쓰기</a>
 </body>
 </html>
